ac7e9a2d68ce3784472d05c08d8c8b004734b8a4
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-04 20:55:23 +0300

    Complete task0504

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0504/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task05.task0504;
+
+
+/* 
+Трикотаж
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat firstCat = new Cat("Valera", 5, 7, 10);
+        Cat secondCat = new Cat("Vitya", 6, 8, 9);
+        Cat thirdCat = new Cat("Kolyma", 7, 7, 12);
+    }
+
+    public static class Cat {
+        private String name;
+        private int age;
+        private int weight;
+        private int strength;
+
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+    }
+}
