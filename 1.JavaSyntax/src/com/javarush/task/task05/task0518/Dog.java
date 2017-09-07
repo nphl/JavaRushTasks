2689dae74ada235a94e8d97d0d8662d81cc48ea2
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-07 21:31:48 +0300

    Complete task0518

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0518/Dog.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task05.task0518;
+
+/* 
+Регистрируем собачек
+*/
+
+
+public class Dog {
+
+    private String name = null;
+    private int height;
+    private String color = null;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    public Dog(String name, int height, String color) {
+        this.name = name;
+        this.height = height;
+        this.color = color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
