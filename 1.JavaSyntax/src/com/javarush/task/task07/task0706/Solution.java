acf9ca59c236ef98ad21a7c598a8d937e00f11b9
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-12 23:25:56 +0300

    Complete task0706

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0706/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task07.task0706;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Улицы и дома
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] array = new int[15];
+        int sumEven = 0, sumOdd = 0;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+            if (i % 2 == 0) {
+                sumEven += array[i];
+            } else {
+                sumOdd += array[i];
+            }
+        }
+        reader.close();
+
+        if (sumEven > sumOdd) {
+            System.out.println("В домах с четными номерами проживает больше жителей.");
+        } else {
+            System.out.println("В домах с нечетными номерами проживает больше жителей.");
+        }
+    }
+}
