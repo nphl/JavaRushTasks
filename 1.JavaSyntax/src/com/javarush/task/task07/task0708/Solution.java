da98cc6c8afacbccc1eabd43201f7c7198b994cd
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-14 23:55:25 +0300

    Complete task0708

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0708/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task07.task0708;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Самая длинная строка
+*/
+
+public class Solution {
+    private static List<String> strings;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        strings = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            strings.add(reader.readLine());
+        }
+
+        reader.close();
+
+        int longestStringLength = strings.get(0).length();
+        for (int i = 1; i < strings.size(); i++) {
+            if (strings.get(i).length() > longestStringLength) {
+                longestStringLength = strings.get(i).length();
+            }
+        }
+
+        for (String s :
+                strings) {
+            if (s.length() == longestStringLength) {
+                System.out.println(s);
+            }
+        }
+
+    }
+}
