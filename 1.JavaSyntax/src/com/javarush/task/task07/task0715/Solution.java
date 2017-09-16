8fae556870466f216d9cfff875e2b2200e854a15
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-16 14:39:24 +0300

    Complete task0715

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task07.task0715;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Продолжаем мыть раму
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("мама");
+        strings.add("мыла");
+        strings.add("раму");
+
+        strings.add(1, "именно");
+        strings.add(3, "именно");
+        strings.add("именно");
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+}
