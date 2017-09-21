a2f3c4893b5e80a3aacd32c50a85488b61f35af5
Konstantin Stepanov <kamael-sp@mail.ru>
2017-09-21 22:35:31 +0300

    Complete task0813

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static HashSet<String> createSet() {
+        //напишите тут ваш код
+        HashSet<String> strings = new HashSet<>();
+        strings.add("Ла"); // 1
+        strings.add("Лабаз"); // 2
+        strings.add("Лабардан"); // 3
+        strings.add("Лабашак"); // 4
+        strings.add("Лабе"); // 5
+        strings.add("Лабет"); // 6
+        strings.add("Лабиау"); // 7
+        strings.add("Лабиринт"); // 8
+        strings.add("Лабиринтотомия"); // 9
+        strings.add("Лаблаш"); // 10
+        strings.add("Лаборантка"); // 11
+        strings.add("Лабота"); // 12
+        strings.add("Лабрадорское"); // 13
+        strings.add("Лабрис"); // 14
+        strings.add("Лабуда"); // 15
+        strings.add("Лабытнанги"); // 16
+        strings.add("Лавадо"); // 17
+        strings.add("Лавальер"); // 18
+        strings.add("Лавандовый"); // 19
+        strings.add("Лаватрина"); // 20
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
