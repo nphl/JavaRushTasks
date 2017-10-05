c6ca066186b7d212301cd02a5dd1fb0da34deeba
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-05 22:04:56 +0300

    Complete task1020

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1020/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task10.task1020;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        reader.close();
+
+        sort(array);
+
+        System.out.println(array[9]);
+        System.out.println(array[10]);
+    }
+
+    public static void sort(int[] array) {
+        //напишите тут ваш код
+        int temp;
+        boolean isSwapped;
+
+        do {
+            isSwapped = false;
+            for (int i = 0; i < array.length - 2; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSwapped = true;
+                }
+            }
+
+            if (!isSwapped) break;
+
+            isSwapped = false;
+            for (int i = array.length - 2; i > -1; i--) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    isSwapped = true;
+                }
+            }
+        } while (isSwapped);
+    }
+}
