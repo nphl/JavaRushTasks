f43fa406929ba8d68b4f9283bbe3bd15aaeba08c
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-15 18:25:49 +0300

    Complete task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String s) {
+
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(double d) {
+
+    }
+
+    public static void print(boolean b) {
+
+    }
+
+    public static void print(Date date) {
+
+    }
+}
