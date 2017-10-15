18367de94da007719a3d9cab2a845a2158030b37
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-15 18:29:21 +0300

    Complete task1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int max(int o1, int o2) {
+        return o1 > o2 ? o1 : o2;
+    }
+
+    public static long max(long o1, long o2) {
+        return o1 > o2 ? o1 : o2;
+    }
+
+    public static double max(double o1, double o2) {
+        return o1 > o2 ? o1 : o2;
+    }
+}
