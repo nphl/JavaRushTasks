7ae50e1e279603ed1f32b36977812c69f7166ed2
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-15 18:24:00 +0300

    Complete task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 522;
+        print(522);
+
+        Integer integer = Integer.valueOf(322);
+        print(integer);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int integer) {
+    }
+
+    public static void print(Integer integer) {
+    }
+}
