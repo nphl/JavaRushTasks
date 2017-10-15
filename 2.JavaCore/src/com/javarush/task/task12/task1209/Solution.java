9337d5b00f6aac09ed19dc912b236ec204e53813
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-15 18:27:44 +0300

    Complete task1209

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int min(int o1, int o2) {
+        return o1 < o2 ? o1 : o2;
+    }
+
+    public static long min(long o1, long o2) {
+        return o1 < o2 ? o1 : o2;
+    }
+
+    public static double min(double o1, double o2) {
+        return o1 < o2 ? o1 : o2;
+    }
+}
