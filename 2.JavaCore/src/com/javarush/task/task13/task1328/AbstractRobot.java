90d80dceb10c402478cb4e4c0dac43f23bbce8f4
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-19 00:15:50 +0300

    Complete task1328

--- 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -1,4 +1,47 @@
 package com.javarush.task.task13.task1328;
 
-public class AbstractRobot {
+import java.util.Random;
+
+public abstract class AbstractRobot implements Attackable, Defensable {
+    private static int hitCount;
+    private static Random random = new Random();
+    protected String name;
+
+    public BodyPart attack() {
+        BodyPart attackedBodyPart = null;
+        // hitCount = hitCount + 1;
+        hitCount = random.nextInt(4) + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.CHEST;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            attackedBodyPart = BodyPart.LEG;
+        }
+        return attackedBodyPart;
+    }
+
+    public BodyPart defense() {
+        BodyPart defencedBodyPart = null;
+        // hitCount = hitCount + 2;
+        hitCount = random.nextInt(4) + 1;
+
+        if (hitCount == 1) {
+            defencedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defencedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 3) {
+            defencedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 4) {
+            hitCount = 0;
+            defencedBodyPart = BodyPart.CHEST;
+        }
+        return defencedBodyPart;
+    }
+
+    public abstract String getName();
 }
