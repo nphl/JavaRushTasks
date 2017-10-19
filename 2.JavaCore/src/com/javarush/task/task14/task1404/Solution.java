8e1c9b15a61d4ea8971ee742b9ce9171330fb44f
Konstantin Stepanov <kamael-sp@mail.ru>
2017-10-19 21:25:13 +0300

    Complete task1404

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1404/Solution.java
@@ -0,0 +1,84 @@
+package com.javarush.task.task14.task1404;
+
+/* 
+Коты
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        // ArrayList<Cat> cats = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder builder = new StringBuilder("");
+        builder.append(reader.readLine());
+        while (!"".equals(builder.toString())) {
+            // cats.add(CatFactory.getCatByKey(builder.toString()));
+            Cat cat = CatFactory.getCatByKey(builder.toString());
+            System.out.println(cat.toString());
+            builder.delete(0, builder.length());
+            builder.append(reader.readLine());
+        }
+        reader.close();
+        /*
+        for (Cat cat : cats) {
+            System.out.println(cat.toString());
+        }
+        */
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat = null;
+            if ("vaska".equals(key)) {
+                cat = new MaleCat("Василий");
+            } else if ("murka".equals(key)) {
+                cat = new FemaleCat("Мурочка");
+            } else if ("kiska".equals(key)) {
+                cat = new FemaleCat("Кисюлька");
+            } else {
+                cat = new Cat(key);
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "Я уличный кот " + getName();
+        }
+    }
+
+    static class MaleCat extends Cat {
+        MaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - солидный кошак по имени " + getName();
+        }
+    }
+
+    static class FemaleCat extends Cat {
+        FemaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - милая кошечка по имени " + getName();
+        }
+    }
+}
