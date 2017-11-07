3a0affc98ea9fe6f255bf61258369ee7c94352e1
Konstantin Stepanov <kamael-sp@mail.ru>
2017-11-07 21:20:25 +0300

    Complete task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    public Solution() {
+
+    }
+
+    private Solution(int i) {
+
+    }
+
+    protected Solution(String s) {
+
+    }
+
+    Solution(Solution solution) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
