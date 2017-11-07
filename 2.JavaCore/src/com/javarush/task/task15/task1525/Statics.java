62aeb986e6dc7a80894728906a621307fd813b3a
Konstantin Stepanov <kamael-sp@mail.ru>
2017-11-07 21:33:20 +0300

    Complete task1525

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "/Users/konstantinstepanov/Library/Mobile Documents/com~apple~CloudDocs/Documents/Programming Projects/IdeaProjects/JavaRushTasks/LICENSE";
+}
