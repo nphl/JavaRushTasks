27bcfe4634a703529d4fcc9368f70a8c1616087c
Konstantin Stepanov <kamael-sp@mail.ru>
2018-02-14 22:12:27 +0300

    Complete task1617

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int countSeconds = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        //add your code here - добавь код тут
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            //add your code here - добавь код тут
+
+            try {
+                for (; countSeconds > 0 || currentThread().isInterrupted(); countSeconds--) {
+                    System.out.printf("%d ", countSeconds);
+                    Thread.sleep(1000);
+                }
+                System.out.print("Марш!");
+            } catch (InterruptedException e) {
+                System.out.print("Прервано!");
+            }
+        }
+    }
+}
